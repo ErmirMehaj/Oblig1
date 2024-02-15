63f494d4f5fc12b6dd471cdf8c33b83495a0defb
Ermir <ermeh8262@oslomet.no>
2024-02-15 18:12:05 +0100

    opprettelse av js og html

---
+++ src/test/java/com/example/oblig1kinobillett/Oblig1KinobillettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1kinobillett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1KinobillettApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
